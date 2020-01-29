d31ad4b4b7dd9adf0d40e945732887474da1dbf1
h-asuka <h_langley_a@yahoo.co.jp>
2020-01-29 16:42:26 +0900

    deploy

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  mount_uploader :image, ImageUploader
+
   has_many :posts, dependent: :destroy #ユーザーがデータベースから削除されてしまった場合にユーザーがした投稿も全て消える
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
-
-  mount_uploader :image, ImageUploader
 
   def already_liked?(post)
     self.likes.exists?(post_id: post.id)

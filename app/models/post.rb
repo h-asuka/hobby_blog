2e5746dadb98773a616f9e84256ba8e25ce6d796
h-asuka <h_langley_a@yahoo.co.jp>
2020-02-03 14:08:33 +0900

    モデル追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ApplicationRecord
   validates :title, :content, presence: true
 
   belongs_to :user
-  has_many :likes, dependent: :destroy
+  has_many :likes, dependent: :destroy #投稿が消えたらお気に入りも消える
   has_many :liked_users, through: :likes, source: :user
+  has_many :comments, dependent: :destroy
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy #ユーザーがデータベースから削除されてしまった場合にユーザーがした投稿も全て消える
   has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
